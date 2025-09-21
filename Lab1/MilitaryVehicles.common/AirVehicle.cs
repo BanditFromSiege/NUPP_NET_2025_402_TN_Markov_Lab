79ad9966a985f34703eae29192bd28644a040208
Ivan Markov <161753485+BanditFromSiege@users.noreply.github.com>
2025-09-21 16:36:28 +0300

    Added classes MilitaryVehicle and others

---
+++ Lab1/MilitaryVehicles.common/AirVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryVehicles.common
+{
+    //Клас для повітряних транспортних засобів
+    public abstract class AirVehicle : MilitaryVehicle
+    {
+        //Статичний конструктор
+        static AirVehicle()
+        {
+            Console.WriteLine("Створено новий клас AirVehicle");
+        }
+
+        //Конструктор
+        public AirVehicle(string model) : base(model) {}
+
+        //Перевизначення методу для запуску двигуна
+        public override void StartEngine()
+        {
+            Console.WriteLine($"Двигун повітряного транспортного засобу {Model} з ідентифікатором {Id} заведено");
+        }
+    }
+}
---
+++ Lab1/MilitaryVehicles.common/MilitaryVehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryVehicles.common
+{
+    public abstract class MilitaryVehicle
+    {
+        public Guid Id { get; set; } //Унікальний ідентифікатор
+        public string Model { get; set; } //Модель транспортного засобу
+
+        //Статичний конструктор
+        static MilitaryVehicle()
+        {
+            Console.WriteLine("Створено новий клас MilitaryVehicle");
+        }
+
+        //Конструктор
+        public MilitaryVehicle(string model)
+        {
+            Id = Guid.NewGuid();
+            Model = model;
+        }
+
+        //Абстрактний метод для запуску двигуна
+        public abstract void StartEngine();
+    }
+}

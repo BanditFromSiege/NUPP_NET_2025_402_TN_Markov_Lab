8f637d59e2fa60d944d42673ef01f1a786432093
Ivan Markov <161753485+BanditFromSiege@users.noreply.github.com>
2025-10-17 19:15:50 +0300

    Added async CRUD interface and class

---
+++ Lab1/MilitaryVehicles.common/CrudServiceAsync.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MilitaryVehicles.common
+{
+    public class CrudServiceAsync<T> : ICrudServiceAsync<T> where T : MilitaryVehicle
+    {
+        private readonly ConcurrentDictionary<Guid, T> elements = new ConcurrentDictionary<Guid, T>();
+        private readonly string filePath;
+
+        public CrudServiceAsync(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public async Task<bool> CreateAsync(T element)
+        {
+            if (elements.ContainsKey(element.Id))
+            {
+                Console.WriteLine($"{element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} вже існує");
+                return false;
+            }
+
+            elements[element.Id] = element;
+            Console.WriteLine($"{element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} доданий");
+            return await SaveAsync();
+        }
+
+        public async Task<T> ReadAsync(Guid id)
+        {
+            if (elements.TryGetValue(id, out T element))
+            {
+                return element;
+            }
+
+            throw new KeyNotFoundException($"Елемент з ідентифікатором {id} не знайдено");
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync()
+        {
+            return elements.Values.ToList();
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync(int page, int amount)
+        {
+            return elements.Values.Skip((page - 1) * amount).Take(amount).ToList();
+        }
+
+        public async Task<bool> UpdateAsync(T element)
+        {
+            if (elements.ContainsKey(element.Id))
+            {
+                elements[element.Id] = element;
+                Console.WriteLine($"{element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} оновлено");
+                return await SaveAsync();
+            }
+
+            Console.WriteLine($"{element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} не знайдено для оновлення");
+            return false;
+        }
+
+        public async Task<bool> RemoveAsync(T element)
+        {
+            if (elements.TryRemove(element.Id, out _))
+            {
+                Console.WriteLine($"{element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} видалено");
+                return await SaveAsync();
+            }
+
+            Console.WriteLine($"Не вдалося знайти {element.GetType().Name} моделі {element.Model} з ідентифікатором {element.Id} для видалення");
+            return false;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(elements.Values.ToList());
+                await File.WriteAllTextAsync(filePath, json);
+                Console.WriteLine("Дані збережені у файл");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка при збереженні даних у файл: {ex.Message}");
+                return false;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return elements.Values.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Lab1/MilitaryVehicles.console/Program.cs
+++ Lab1/MilitaryVehicles.console/Program.cs
@@ -4,141 +4,41 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        List<Tank> SovietTanks = new List<Tank> {
-            new Tank("T-72"),
-            new Tank("T-64"),
-            new Tank("T-80"),
-        };
+        var tank = new Tank("T-72");
+        var helicopter = new Helicopter("Mi-24");
+        var destroyer = new Destroyer("Project 7");
 
-        List<Tank> USTanks = new List<Tank> {
-            new Tank("M1 Abrams"),
-            new Tank("M60 Patton"),
-            new Tank("M4 Sherman"),
-        };
-
-        List<Helicopter> SovietHelicopters = new List<Helicopter> {
-            new Helicopter("Mi-24"),
-            new Helicopter("Mi-8"),
-            new Helicopter("Ka-50"),
-        };
-
-        List<Helicopter> USHelicopters = new List<Helicopter> {
-            new Helicopter("UH-60 Black Hawk"),
-            new Helicopter("AH-64 Apache"),
-            new Helicopter("CH-47 Chinook"),
-        };
-
-        List<Destroyer> SovietDestroyers = new List<Destroyer> {
-            new Destroyer("Project 7"),
-            new Destroyer("Project 956"),
-            new Destroyer("Project 1155"),
-        };
-
-        List<Destroyer> USDestroyers = new List<Destroyer> {
-            new Destroyer("USS Arleigh Burke"),
-            new Destroyer("USS Kidd"),
-            new Destroyer("USS John Paul Jones"),
-        };
+        var filePath = "vehicles.json";
+        var crudServiceAsync = new CrudServiceAsync<MilitaryVehicle>(filePath);
 
         Console.WriteLine();
 
-        SovietTanks[0].EngineStarted += (sender) => 
-            Console.WriteLine($"Час запуску {sender.Model}: {DateTime.Now}");
-
-        SovietTanks[0].StartEngine();
-
-        SovietHelicopters[0].EngineStarted += (sender) =>
-            Console.WriteLine($"Час запуску {sender.Model}: {DateTime.Now}");
-
-        SovietHelicopters[0].StartEngine();
-
-        SovietDestroyers[0].EngineStarted += (sender) =>
-            Console.WriteLine($"Час запуску {sender.Model}: {DateTime.Now}");
-
-        SovietDestroyers[0].StartEngine();
+        await crudServiceAsync.CreateAsync(tank);
+        await crudServiceAsync.CreateAsync(helicopter);
+        await crudServiceAsync.CreateAsync(destroyer);
 
         Console.WriteLine();
 
-        SovietTanks[0].Fire();
-        SovietHelicopters[0].TakeOff();
-        SovietDestroyers[0].FireGuns();
-
-        Console.WriteLine();
-
-        var CrudService = new CrudService<MilitaryVehicle>();
-
-        foreach (var tank in SovietTanks)
+        var allVehicles = await crudServiceAsync.ReadAllAsync();
+        foreach (var vehicle in allVehicles)
         {
-            CrudService.Create(tank);
-        }
-        Console.WriteLine();
-
-        foreach (var tank in USTanks)
-        {
-            CrudService.Create(tank);
-        }
-        Console.WriteLine();
-
-        foreach (var chopper in SovietHelicopters)
-        {
-            CrudService.Create(chopper);
-        }
-        Console.WriteLine();
-
-        foreach (var chopper in USHelicopters)
-        {
-            CrudService.Create(chopper);
-        }
-        Console.WriteLine();
-
-        foreach (var destroyer in SovietDestroyers)
-        {
-            CrudService.Create(destroyer);
-        }
-        Console.WriteLine();
-
-        foreach (var destroyer in USDestroyers)
-        {
-            CrudService.Create(destroyer);
-        }
-        Console.WriteLine();
-
-        Console.WriteLine($"Всього створено техніки: { MilitaryVehicle.GetVehicleCount() }");
-
-        Console.WriteLine();
-
-        try
-        {
-            var foundTank = CrudService.Read(SovietTanks[0].Id);
-            Console.WriteLine($"Знайдено танк: {foundTank.Model} з ідентифікатором {foundTank.Id}\n");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
+            vehicle.PrintInfo();
         }
 
-        CrudService.Remove(SovietTanks[0]);
+        Console.WriteLine();
 
-        SovietTanks[1].Model = "T-34";
-        CrudService.Update(SovietTanks[1]);
-
-        Console.WriteLine("\nПерелік всієї техніки:");
-        int i = 0;
-        foreach (var el in CrudService.ReadAll())
+        var paginatedVehicles = await crudServiceAsync.ReadAllAsync(1, 2);
+        foreach (var vehicle in paginatedVehicles)
         {
-            el.PrintInfo();
-            ++i;
-            if (i % 3 == 0)
-            {
-                Console.WriteLine();
-            }
+            vehicle.PrintInfo();
         }
 
-        Console.WriteLine("\nНатисніть, щоб завершити програму...");
-        Console.ReadLine();
+        Console.WriteLine();
+
+        await crudServiceAsync.RemoveAsync(tank);
     }
 }

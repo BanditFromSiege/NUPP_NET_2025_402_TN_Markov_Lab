9ca47deb51e7c27a9ded3f9c231384037b11e799
Ivan Markov <161753485+BanditFromSiege@users.noreply.github.com>
2025-10-30 03:22:29 +0200

    Completed task 4 of Lab 3

---
+++ Lab1/MilitaryVehicles.infrastructure/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MilitaryVehicles.infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MilitaryVehiclesContext context;
+        private readonly DbSet<T> dbSet;
+
+        public Repository(MilitaryVehiclesContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            dbSet.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
